f7e8944fad340913d0993dd6949bd307d916dca4
xisigui <nagato.leet@gmail.com>
2025-03-30 01:07:08 +0800

    fix: fix bug where screen shows nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { PreviewImageDialog } from "./components/preview-image";
 
 function App() {
+  // @ts-ignore
+  window.electron.getAllItems();
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <h1>Hello World</h1>
+      <div className="m-screen h-screen p-3">
+        <div className="grid grid-cols-2 gap-2 w-full h-full ">
+          <div className="flex items-center justify-center">
+            <PreviewImageDialog />
+          </div>
+          <div className="flex items-center justify-center">
+            <PreviewImageDialog />
+          </div>
+          <div className="flex items-center justify-center">
+            <PreviewImageDialog />
+          </div>
+          <div className="flex items-center justify-center">
+            <PreviewImageDialog />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,11 +1,23 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "path";
+import { getPreloadPath } from "./pathResolver.js";
+import { isDev } from "./util.js";
+import { getAllItems } from "./dataManager.js";
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
-    frame: false,
+    width: 1195,
+    height: 950,
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
   });
-  mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  mainWindow.setMenuBarVisibility(false);
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html"));
+  }
 });
 
 app.on("window-all-closed", () => {
