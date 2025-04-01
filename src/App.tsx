bd1972e6cf39b003011782b65589cf7bbc4124c5
xisigui <nagato.leet@gmail.com>
2025-04-01 21:47:23 +0800

    feat: Added ui sample design

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,133 @@
 import "./App.css";
 import { PreviewImageDialog } from "./components/preview-image";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function App() {
   // @ts-ignore
   window.electron.getAllItems();
+
+  const sampleData = [
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBzdW5zZXR8ZW58MHx8MHx8fDA%3D",
+      title: "Beach Sunset",
+      description: "A beautiful sunset at the beach.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1615809265087-1416ccddd6ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW4lMjB2aWV3fGVufDB8fDB8fHww",
+      title: "Mountain View",
+      description: "Stunning view of the mountains during sunset.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351241-b1b2ccae70c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8",
+      title: "City Skyline",
+      description: "A breathtaking city skyline at night.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742201835989-4e346e36b364?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8",
+      title: "Forest Adventure",
+      description: "Exploring the deep forest on a bright day.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742054294284-baa5691ede46?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Winter Wonderland",
+      description: "Snowy winter landscape with a serene atmosphere.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+    {
+      imgUrl:
+        "https://images.unsplash.com/photo-1742268351428-ba1366f7dae3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D",
+      title: "Desert Oasis",
+      description: "A peaceful oasis in the middle of the desert.",
+    },
+  ];
   return (
     <>
-      <div className="m-screen h-screen p-3">
-        <div className="grid grid-cols-2 gap-2 w-full h-full ">
-          <div className="flex items-center justify-center">
-            <PreviewImageDialog />
-          </div>
-          <div className="flex items-center justify-center">
-            <PreviewImageDialog />
-          </div>
-          <div className="flex items-center justify-center">
-            <PreviewImageDialog />
-          </div>
-          <div className="flex items-center justify-center">
-            <PreviewImageDialog />
-          </div>
+      <ScrollArea className="h-screen p-4">
+        <div className="grid grid-cols-4 gap-4 w-full h-full">
+          {sampleData.map((item) => (
+            <PreviewImageDialog
+              imgUrl={item.imgUrl}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
         </div>
-      </div>
+        <Dialog>
+          <DialogTrigger>
+            <Button
+              className="fixed bottom-4 right-4 z-1080"
+              variant="outline"
+              size="icon"
+            >
+              <CirclePlus size={48} strokeWidth={1.5} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create New Item</DialogTitle>
+            </DialogHeader>
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="title">Title</Label>
+              <Input id="title" />
+            </div>
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="description">Description</Label>
+              <Input id="description" />
+            </div>
+            <div className="grid w-full items-center gap-1.5">
+              <Label htmlFor="pictureUrl">Picture URL</Label>
+              <Input id="pictureUrl" />
+            </div>
+            <Button className="mt-3 p-4">Save</Button>
+          </DialogContent>
+        </Dialog>
+      </ScrollArea>
     </>
   );
 }
--- src/components/preview-image.tsx
+++ src/components/preview-image.tsx
@@ -1,67 +1,25 @@
-import {
-  MorphingDialog,
-  MorphingDialogTrigger,
-  MorphingDialogContent,
-  MorphingDialogClose,
-  MorphingDialogImage,
-  MorphingDialogContainer,
-} from "@/components/core/morphing-dialog";
-import { XIcon } from "lucide-react";
-import { ProgressiveBlur } from "@/components/core/progressive-blur";
+import { Card, CardContent } from "@/components/ui/card";
 
-export function PreviewImageDialog() {
+export function PreviewImageDialog({ imgUrl, title, description }) {
   return (
-    <MorphingDialog
-      transition={{
-        duration: 0.2,
-        ease: "easeInOut",
-      }}
-    >
-      <MorphingDialogTrigger>
-        <div className="relative my-4 aspect-square w-[300px] overflow-hidden rounded-[4px]">
-          <MorphingDialogImage
-            src="https://cdn.cosmos.so/c4653e73-d082-42e9-87d2-5377d7e6a7f3?format=jpeg"
-            alt="Sony Style Store in the Sony Center complex - Berlin, Germany (2000)"
-            className="absolute inset-0"
+    <Card className="py-0 gap-0 rounded-lg">
+      <CardContent className="px-0">
+        <div className="overflow-hidden rounded-t-lg">
+          <img
+            src={imgUrl}
+            alt="Product Image"
+            width={600}
+            height={400}
+            className="w-full h-64 object-cover"
+            style={{ aspectRatio: "600/400", objectFit: "cover" }}
           />
-          <ProgressiveBlur
-            className="pointer-events-none absolute bottom-0 left-0 h-[75%] w-full"
-            blurIntensity={1}
-          />
-          <div className="absolute bottom-0 left-0">
-            <div className="flex flex-col items-start gap-0 px-5 py-4">
-              <p className="text-base font-medium text-white">
-                Benjamin Spiers
-              </p>
-              <span className="mb-2 text-base text-zinc-300">
-                Moonlight 2023
-              </span>
-            </div>
-          </div>
         </div>
-      </MorphingDialogTrigger>
-      <MorphingDialogContainer>
-        <MorphingDialogContent className="relative">
-          <MorphingDialogImage
-            src="https://cdn.cosmos.so/c4653e73-d082-42e9-87d2-5377d7e6a7f3?format=jpeg"
-            alt="Sony Style Store in the Sony Center complex - Berlin, Germany (2000)"
-            className="aspect-square h-auto w-full max-w-[90vw] rounded-[4px] object-cover lg:h-[90vh]"
-          />
-        </MorphingDialogContent>
-        <MorphingDialogClose
-          className="fixed right-6 top-6 h-fit w-fit rounded-full bg-white p-1"
-          variants={{
-            initial: { opacity: 0 },
-            animate: {
-              opacity: 1,
-              transition: { delay: 0.2, duration: 0.1 },
-            },
-            exit: { opacity: 0, transition: { duration: 0 } },
-          }}
-        >
-          <XIcon className="h-5 w-5 text-zinc-500" />
-        </MorphingDialogClose>
-      </MorphingDialogContainer>
-    </MorphingDialog>
+
+        <div className="p-6 space-y-2">
+          <h3 className="text-xl font-semibold">{title}</h3>
+          <p className="text-gray-500 dark:text-gray-400">{description}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
